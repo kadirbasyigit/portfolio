c2f0341ebe9f38dd75ba79e3038e8e29bcafde96
k1d12r04 <k1d12r04@gmail.com>
2024-10-27 00:49:20 +0300

    Refactor footer component and update social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,55 +1,62 @@
 import { Button } from './ui/button';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa6';
 
+type RenderLinkProps = {
+  href: string;
+  text?: string;
+  icon?: JSX.Element;
+};
+
 const Footer = () => {
+  const links: RenderLinkProps[] = [
+    {
+      href: 'https://github.com/kadirbasyigit/portfolio',
+      text: 'this github repository.',
+    },
+    {
+      href: 'https://github.com/kadirbasyigit',
+      icon: (
+        <FaGithub className="w-7 h-7 hover:text-[#3333ff] dark:text-white/90 dark:hover:text-[#7ce8be] transition" />
+      ),
+    },
+    {
+      href: 'https://www.linkedin.com/in/kadirbasyigitt',
+      icon: (
+        <FaLinkedinIn className="w-7 h-7 hover:text-[#3333ff] dark:text-white/90 dark:hover:text-[#7ce8be] transition" />
+      ),
+    },
+  ];
+
+  const renderLink = ({ href, text, icon }: RenderLinkProps) => (
+    <Button
+      className="p-0 m-0 rounded-none font-medium h-auto"
+      variant="link"
+      asChild
+    >
+      <a
+        className={`relative p-0 m-0 ${
+          text ? 'footer-link' : ''
+        } dark:text-white/90`}
+        href={href}
+        target="_blank"
+      >
+        {text || icon}
+      </a>
+    </Button>
+  );
+
   return (
     <div className="relative overflow-hidden">
       <section className="bg-white dark:bg-black dark:text-white/90 shadow-2xl py-6 rounded-md font-sora text-sm my-16 space-y-6 w-11/12 lg:max-w-[1000px] mx-auto">
         <p className="text-center">
           You can find the source code for this site at &nbsp;
-          <Button
-            className="p-0 m-0 rounded-none font-medium h-auto"
-            variant="link"
-            asChild
-          >
-            <a
-              className="relative p-0 m-0 footer-link dark:text-white/90"
-              href="https://github.com/k1d12r04/portfolio"
-              target="_blank"
-            >
-              this github repository.
-            </a>
-          </Button>
+          {renderLink(links[0])}
         </p>
 
         <p className="flex items-center justify-center gap-4">
           Reach me out on
-          <Button
-            className="p-0 m-0 rounded-none font-medium h-auto"
-            variant="link"
-            asChild
-          >
-            <a
-              className="relative p-0 m-0"
-              href="https://github.com/k1d12r04"
-              target="_blank"
-            >
-              <FaGithub className="w-7 h-7  hover:text-[#3333ff] dark:text-white/90 dark:hover:text-[#3333ff] transition  hover:border-blue-600 " />
-            </a>
-          </Button>
-          <Button
-            className="p-0 m-0 rounded-none font-medium h-auto"
-            variant="link"
-            asChild
-          >
-            <a
-              className="relative p-0 m-0"
-              href="https://www.linkedin.com/in/k1d12r"
-              target="_blank"
-            >
-              <FaLinkedinIn className="w-7 h-7  hover:text-[#3333ff] dark:text-white/90 dark:hover:text-[#3333ff] transition  hover:border-blue-600 " />
-            </a>
-          </Button>
+          {renderLink(links[1])}
+          {renderLink(links[2])}
         </p>
       </section>
       <div className="wave"></div>
